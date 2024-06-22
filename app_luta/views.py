0d002e56714bdccdf574e6dd587c20bd1eb8ec14
BOCCOLLO <you@example.com>
2024-06-22 16:13:00 -0300

    refctore: alter paginas novas

--- app_luta/views.py
+++ app_luta/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def organizador(request):
+    return render(request, 'organizador.html')
+
+def academia(request):
+    return render(request, 'academia.html')
--- proj_luta_2/urls.py
+++ proj_luta_2/urls.py
@@ -5,4 +5,8 @@ from app_luta import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'), 
+    path('index/organizador', views.organizador, name='organizador'),
+    path('index/academia', views.academia, name='academia'), 
+
+
 ]
