458c5159ed7ca94b58918c691667edbc836de924
ryanboccollo <ryan.b.bueno7@gmail.com>
2024-11-16 03:46:25 -0300

    AAAAAAAAAAAAAAAAAAAAAA

--- app_luta/views.py
+++ app_luta/views.py
@@ -15,3 +15,21 @@ def cadastro_academia(request):
     
 def academia_hub(request):
     return render(request, 'academia_hub.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def cadastrar_lutador(request):
+    if request.method == 'POST':
+        form = LutadorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_lutadores')  # Redireciona para a lista de lutadores
+    else:
+        form = LutadorForm()
+    return render(request, 'cadastro_lutador.html', {'form': form})
+
+# View para listar os lutadores cadastrados
+def listar_lutadores(request):
+    lutadores = Lutador.objects.all()
+    return render(request, 'listar_lutadores.html', {'lutadores': lutadores})
--- proj_luta_2/urls.py
+++ proj_luta_2/urls.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
 from django.urls import path
-from app_luta import views 
+from django.contrib.auth.views import LoginView  # Importação necessária
+from app_luta import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'), 
-    path('index/organizador', views.organizador, name='organizador'),
-    path('index/academia', views.academia, name='academia'), 
-    path('academia/cadastro_academia', views.cadastro_academia, name='cadastro_academia'),
-    path('academia/academia_hub', views.academia_hub, name='academia_hub'),
+    # Admin Routes
+    path('admin/', admin.site.urls, name='admin'),
+
+    # Home Page
+    path('', views.index, name='index'),
+
+    # Organizador Routes
+    path('organizador/', views.organizador, name='organizador'),
+
+    # Academia Routes
+    path('academia/', views.academia, name='academia'),
+    path('academia/cadastro/', views.cadastro_academia, name='cadastro_academia'),
+
+    # Login Routes
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    
+    # Academia Hub (depois de login)
+    path('academia/hub/', views.academia_hub, name='academia_hub'),
+
+    path('academia/hub/cadastro_lutador/', views.cadastro_lutador, name='cadastro_lutador'),
 ]
